d1d1443e77ff0886bafb686d21d8e30e21c31f97
Harikrishna <98644822+Harikrishna-kvn@users.noreply.github.com>
2023-12-27 16:15:02 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,6 +1,26 @@
 variable "region" {}
+#network variable
 variable "vpc_cidr" {}
-variable "vpc_name" {}
+#variable "vpc_name" {}
 variable "public_subnets" {}
 variable "ap_availability_zone" {}
 variable "private_subnets" {}
+
+#security variable
+
+
+variable "ec2_sg_name" {}
+variable "ec2_jenkins_sg_name" {}
+
+#jenkins-server variables
+
+variable "ami_id" {}
+variable "instance_type" {}
+variable "tag_name" {}
+variable "public_key" {}
+/*variable "subnet_id" {}
+variable "sg_for_jenkins" {}
+variable "enable_public_ip_address" {}
+variable "user_data_install_jenkins" {}*/
+
+
